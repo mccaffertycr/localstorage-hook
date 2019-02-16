2da21cdd2373a59b896cff77485d1fcd7c9eb7c5
mccaffertycr <mccaffertycr@gmail.com>
2019-02-16 16:22:19 -0500

    setting state to localstorage

--- src/index.js
+++ src/index.js
@@ -1,5 +1,28 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
+import React, { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
 
-ReactDOM.render(<div>yay hooks</div>, document.getElementById("app"));
+function App() {
+  const [count, setCount] = useState(() => {
+    let value;
+    try {
+      value = JSON.parse(window.localStorage.getItem('react-app:count') || '0');
+    } catch (e) {
+      // ignore error
+    }
+    return value;
+  });
+
+  useEffect(() => {
+    window.localStorage.setItem('react-app:count', count);
+  }, [count]);
+
+  return (
+    <>
+      <button onClick={() => setCount(0)}>clear</button>
+      <button onClick={() => setCount(count + 1)}>{count}</button>
+    </>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
